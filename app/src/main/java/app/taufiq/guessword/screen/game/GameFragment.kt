189a08c668faf4bc63f0f5e5350175ca1045d065
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-08-05 09:08:24 +0800

    create game list of word

--- app/src/main/java/app/taufiq/guessword/screen/game/GameFragment.kt
+++ app/src/main/java/app/taufiq/guessword/screen/game/GameFragment.kt
@@ -12,17 +12,103 @@ import app.taufiq.guessword.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
+    // current word
+    private var word = " "
+
+    // current score
+    private var score = 0
+
+    // the list of word - the front of the list is the next word  to guess
+    private lateinit var listOfWord: MutableList<String>
+
+    // get view
+    private lateinit var binding: FragmentGameBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentGameBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_game,container,false)
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        resetLists()
+        nextWord()
+
+        binding.buttonSkip.setOnClickListener { onSkip() }
+        binding.buttonGotIt.setOnClickListener { onCorrect() }
+
+        updateScore()
+        updateWord()
+
         return binding.root
     }
 
 
+    // list of the words
+    private fun resetLists() {
+        listOfWord = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        listOfWord.shuffle()
+    }
+
+
+    // method for buttons preset
+    private fun onSkip() {
+        score--
+        nextWord()
+
+    }
+
+    private fun onCorrect() {
+        score++
+        nextWord()
+    }
+
+
+    /*
+    * Method for the next word
+    * */
+    private fun nextWord() {
+        if (!listOfWord.isEmpty()) {
+            // select and remove a word from the list
+            word = listOfWord.removeAt(0)
+        }
+
+        // give a new word
+        updateWord()
+        updateScore()
+    }
+
+    private fun updateScore() {
+        binding.scoreText.text = score.toString()
+    }
+
+    private fun updateWord() {
+        binding.wordText.text = word
+    }
 
 
 }
